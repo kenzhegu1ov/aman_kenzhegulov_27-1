e63e5e52e95bf8d35f8c72561626291597c71edd
kenzhegu1ov <amantajkenzhegulov@gmail.com>
2023-05-04 16:37:44 +0600

    Initial commit

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,6 @@ from django.db import models
 class Products(models.Model):
     image = models.ImageField()
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200, default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rate = models.FloatField(default=5)
     cpu = models.CharField(max_length=50)
@@ -23,7 +22,5 @@ class Review(models.Model):
     text = models.CharField(max_length=256)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
 
-
     def __str__(self):
         return self.text
-
